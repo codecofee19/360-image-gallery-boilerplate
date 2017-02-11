cad67b1f0e31c1d4b900c6e8e84edb368a3ef28c
Harold Jackson <xela2407@gmail.com>
2017-02-11 15:10:42 -0600

    update js

--- components/set-image.js
+++ components/set-image.js
@@ -24,7 +24,7 @@ AFRAME.registerComponent('set-image', {
       // Wait for fade to complete.
       setTimeout(function () {
         // Set image.
-        data.target.setAttribute('material', 'src', data.src);
+        data.target.setAttribute('src', 'beyonce.jpeg');
       }, data.dur);
     });
   },
